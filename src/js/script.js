c1736c52757cc7c6557524755983dcbbf201144a
annagna <annagna08@gmail.com>
2026-04-04 00:14:43 -0300

    foi colocado alternativas para tipos de vinhos

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,18 @@
     var Nome = prompt('Digite seu nome: ');
     var Sobrenome = prompt('Digite seu sobrenome: ');
     var Email = prompt('Digite seu email: ');
-    alert("Cadastro realizado com sucesso!");
+        if (Email === null || Email.trim() === '') {
+            alert('Email não inserido, favor colocar um email válido.');
+            return;
+        } else if (!Email.includes('@') || !Email.includes('.')) {
+            alert('Email inválido, favor colocar um email válido.');
+            return;
+        } else {
+            alert("Cadastro realizado com sucesso! Bem-vindo," + Nome + "!");
+        }
+    
   });
+
+
+
+
